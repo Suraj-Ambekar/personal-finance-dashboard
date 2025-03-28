5ac783fab9090d14f2cae2db46128efbc39967b3
Suraj Ambekar <ambekarsuraj03@gmail.com>
2025-03-28 21:04:35 +0530

    updated keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// // import { getAnalytics } from "firebase/analytics";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDbLP6XLJpstxFbL0yZ3SDtHjWt25GG_pY",
-//   authDomain: "personal-finance-dashboa-bf30f.firebaseapp.com",
-//   projectId: "personal-finance-dashboa-bf30f",
-//   storageBucket: "personal-finance-dashboa-bf30f.firebasestorage.app",
-//   messagingSenderId: "757863983361",
-//   appId: "1:757863983361:web:11e7ab45cfba0e3502d300",
-//   measurementId: "G-NJ28J7S9PS",
-// };
-
-// // // Initialize Firebase
-// // const app = initializeApp(firebaseConfig);
-// // // const analytics = getAnalytics(app);
-// // export const auth = getAuth(app);
-// // export default app;
-// // export const db = getFirestore(app);
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
